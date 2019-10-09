ba006594ff536fd605160ea028b355e6706ce7dd
starragop <39194041+starragop@users.noreply.github.com>
2019-10-09 16:20:14 -0400

    Update and rename main.py to listen.py

--- listen.py
+++ listen.py
@@ -1,20 +1,17 @@
 import socket
 import sys
-import cmds
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('0.0.0.0', 5566)
 sock.bind(server_address)
 sock.listen(1)
-BUFFERSIZE=128
+BUFFERSIZE=1024
 
 while True:
     connection, client_address = sock.accept()
     try:
         while True:
             data = connection.recv(BUFFERSIZE)
-            function=getattr(cmds,data)
-            function(arg1)
             if data:
                 connection.sendall(data)
             else:
