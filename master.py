f8c2b273405727ac027bfa030ba8589094e5f4c6
starragop <39194041+starragop@users.noreply.github.com>
2019-10-09 15:18:07 -0400

    Update master.py

--- master.py
+++ master.py
@@ -1,15 +1,20 @@
 import socket
-import sys
 
 IP = '192.168.88.24'
 PORT = 5566
 BUFFERSIZE = 1024
-command=sys.argv[1]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(command)
-data = s.recv(BUFFERSIZE)
+print('~~CONNECTED~~')
+while True:
+    userinput = input('->')
+    if userinput != '':
+        s.send(command)
+        data = s.recv(BUFFERSIZE)
+        if data == True:
+            print(data)
+        else:
+            break
 s.close()
-
-print "Command sent: ", data
+print('~~CONNECTION CLOSED~~')
